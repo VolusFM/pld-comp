fcb349ab18e2c4f34e0d33a14218c2a006da410a
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-03-14 20:13:11 +0100

    improvments

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
 #include <fstream>
-#include <sstream>
+using std::ifstream;
 
 #include "antlr4-runtime.h"
 #include "code_antlr/CodeCLexer.h"
 #include "code_antlr/CodeCParser.h"
 #include "code_antlr/CodeCVisitor.h"
+
 #include "code_visitors/Visitor.h"
 #include "code_visitors/CProg.h"
 
 using namespace antlr4;
-using namespace std;
 
 int main(int argc, const char* argv[]) {
-
-  if (argc != 2) {
-    cout << "Syntaxe : exe <fichier.c>" << endl;
+  
+  if (argc < 2) {
+    cerr << "Error: missing input file parameter." << endl;
     exit(EXIT_FAILURE);
-  } 
-
-  stringstream in; 
-  if (argc == 2) {
-    ifstream fichier(argv[1]);
-    in << fichier.rdbuf();
   }
-
-  ANTLRInputStream input(in.str());
+  
+  ifstream stream(argv[1]);
+  
+  ANTLRInputStream input(stream);
   CodeCLexer lexer(&input);
   CommonTokenStream tokens(&lexer);
   
@@ -33,7 +32,7 @@ int main(int argc, const char* argv[]) {
   tree::ParseTree* tree = parser.prog();
   
   Visitor visitor;
-  CProg * resultat = visitor.visit(tree);
+  CProg* resultat = visitor.visit(tree);
   cout << resultat->to_asm() << endl;
   
   return 0;
