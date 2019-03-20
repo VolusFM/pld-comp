9da1b0faad8cb6af70a94bc2e59f801f195d0d57
FrancoisMontigny <francois.montigny@gmail.com>
2019-03-20 10:35:10 +0100

    chore: refactoring and memory leaks checks

--- source/AST/CInstrReturn.cpp
+++ source/AST/CInstrReturn.cpp
@@ -3,16 +3,29 @@
 #include <string>
 using std::to_string;
 
+/*
+ * Default constructor - If there is no return (return void),
+ * set expr to nullptr to be able to recognize it.
+ */
 CInstrReturn::CInstrReturn() :
         expr(nullptr) {
+
+}
+
+CInstrReturn::CInstrReturn(CExpression * expr) :
+        expr(expr) {
+}
+
+CInstrReturn::~CInstrReturn() {
+    delete expr;
 }
 
 string CInstrReturn::to_asm() const {
     string code;
 
-    int* returnValue = nullptr;
-    if (dynamic_cast<CExpressionInt*>(expr) != nullptr) {
-        returnValue = &dynamic_cast<CExpressionInt*>(expr)->value;
+    int * returnValue = nullptr;
+    if (dynamic_cast<CExpressionInt *>(expr) != nullptr) {
+        returnValue = &dynamic_cast<CExpressionInt *>(expr)->value;
     }
 
     if (returnValue != nullptr) {
