be7a3fcf38a4c5c09f47a7f416ed2fb2e059e073
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-03-20 12:13:50 +0100

    merge Visitor.h

--- source/AST/Visitor.h
+++ source/AST/Visitor.h
@@ -18,16 +18,18 @@ using std::vector;
 class Visitor : public CodeCBaseVisitor
 {
 public:
-  virtual antlrcpp::Any visitProg(CodeCParser::ProgContext *ctx) override
-  {
-    CProg prog;
-
-    for (auto ctx_func : ctx->function())
+    virtual antlrcpp::Any visitProg(CodeCParser::ProgContext *ctx) override
     {
-      prog.functions.push_back(*((CFunction *)visit(ctx_func)));
+        CProg prog;
+        
+        for (auto ctx_func : ctx->function())
+        {
+            prog.functions.push_back(*((CFunction *)visit(ctx_func)));
+        }
+        
+        return new CProg(prog);
     }
-
-/*<<<<<<< HEAD
+    
     virtual antlrcpp::Any visitFunction(CodeCParser::FunctionContext *ctx)
             override {
         string name = visit(ctx->functionheader());
@@ -52,9 +54,9 @@ public:
         CInstructions* block = new CInstructions(*instructions);
         // This pointer has been used, we may delete it to avoid memory leaks.
         // delete instructions;
-        /*bloc->instructions = (vector<CInstruction*>) visit(
-                        ctx->instructions()));*/
-        /*return block;
+        // bloc->instructions = (vector<CInstruction*>)
+        //                      visit(ctx->instructions()));
+        return block;
     }
 
     virtual antlrcpp::Any visitInstructions(
@@ -92,75 +94,6 @@ public:
             override {
         return (CInstruction*) ((CInstrVariable*) visit(ctx->vardefinition()));
     }
-=======
-    return new CProg(prog);
-  }
->>>>>>> fd94b1d06a7a7a583d10142bb1bf0fc4cfa2a0bd*/
-
-  virtual antlrcpp::Any visitFunction(CodeCParser::FunctionContext *ctx)
-      override
-  {
-    string name = *((string *)visit(ctx->functionheader()));
-    CInstructions * block = (CInstructions *) visit(
-                ctx->instructionsbloc());
-    CFunction *func = new CFunction(name, block);
-    func->fill_tos();
-    return func;
-  }
-
-  virtual antlrcpp::Any visitFunctionheader(
-      CodeCParser::FunctionheaderContext *ctx) override
-  {
-    return new string(ctx->IDENT()->getText());
-  }
-
-  virtual antlrcpp::Any visitInstructionsbloc(
-      CodeCParser::InstructionsblocContext *ctx) override
-  {
-    CInstructions *bloc = new CInstructions();
-    bloc->instructions = *((vector<CInstruction *> *)visit(
-        ctx->instructions()));
-    return bloc;
-  }
-
-  virtual antlrcpp::Any visitInstructions(
-      CodeCParser::InstructionsContext *ctx) override
-  {
-    vector<CInstruction *> instructions;
-
-    for (auto ctx_instr : ctx->instruction())
-    {
-      instructions.push_back((CInstruction *)visit(ctx_instr));
-    }
-
-    return new vector<CInstruction *>(instructions);
-  }
-
-  virtual antlrcpp::Any visitReturn(CodeCParser::ReturnContext *ctx)
-      override
-  {
-    return (CInstruction *)((CInstrReturn *)visit(ctx->instrreturn()));
-  }
-
-  virtual antlrcpp::Any visitReturn_expr(CodeCParser::Return_exprContext *ctx)
-      override
-  {
-    CInstrReturn *instr = new CInstrReturn();
-    instr->expr = (CExpression *)visit(ctx->expression());
-    return instr;
-  }
-
-  virtual antlrcpp::Any visitReturn_void(CodeCParser::Return_voidContext *ctx)
-      override
-  {
-    return new CInstrReturn();
-  }
-
-  virtual antlrcpp::Any visitInstr_def(CodeCParser::Instr_defContext *ctx)
-      override
-  {
-    return (CInstruction *)((CInstrVariable *)visit(ctx->vardefinition()));
-  }
 
   virtual antlrcpp::Any visitDef_var(CodeCParser::Def_varContext *ctx)
       override {
@@ -253,13 +186,16 @@ public:
         return (CExpression*) expr;
     }
 
-  /*  virtual antlrcpp::Any visitNeg_expr(CodeCParser::Neg_exprContext *ctx) override
-  {
-    CExpression *rhs = (CExpression *)visit(ctx->expression());
-    CExpression *lhs = new CExpressionInt(0);
-
-    CExpressionComposed *expr = new CExpressionComposed(
-        (CExpression *)lhs, "-", (CExpression *)rhs);
-    return (CExpression *)expr;
-  }*/
+  /*
+    virtual antlrcpp::Any visitNeg_expr(CodeCParser::Neg_exprContext *ctx) override
+    {
+        CExpression *rhs = (CExpression *)visit(ctx->expression());
+        CExpression *lhs = new CExpressionInt(0);
+        
+        CExpressionComposed *expr = new CExpressionComposed(
+            (CExpression *)lhs, "-", (CExpression *)rhs);
+        return (CExpression *)expr;
+    }
+  */
 };
+
