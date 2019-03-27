7e2cffa144f2b0ec36af240a9053311ecec124f9
Kragzmo-Simon <39999784+Kragzmo-Simon@users.noreply.github.com>
2019-03-27 11:52:31 +0100

    Tests on comments and, separated decl/def and complex expressions

---
+++ tests/Test03/input.c
@@ -0,0 +1,7 @@
+
+int main() {
+    int a = 36;
+    int b = 12;
+    int c = 5;
+    return 4*a-c+b/6;
+}
---
+++ tests/Test05/input.c
@@ -0,0 +1,9 @@
+
+int main() {
+    int a = 36;
+    int b;
+    b = 12;
+    int c = 1 + 2 * 2;
+    
+    return 4*a-c+b/6;
+}
