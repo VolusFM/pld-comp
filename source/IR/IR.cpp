96fef9b14cac3c366efa42d5ca716c74117bab7f
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-04-08 17:23:27 +0200

    fix incorrect optimization if branch transformed into jump if empty basic blocs

--- source/IR/IR.cpp
+++ source/IR/IR.cpp
@@ -135,6 +135,7 @@ void CFG::optimize() {
     for (auto itr = bbReplace.begin(); itr != bbReplace.end(); ++itr) {
         for (auto it = bbs.begin(); it != bbs.end(); ++it) {
             BasicBlock* b = (*it);
+            if (b->exit_false != nullptr && itr->second == nullptr) continue;
             if (b->exit_true == itr->first) b->exit_true = itr->second;
             if (b->exit_false == itr->first) b->exit_false = itr->second;
         }
