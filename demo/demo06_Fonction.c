ad3f800c5255f489d302b6432aed2cd348e5ae6e
Spherebleue <spherebleue@gmail.com>
2019-04-10 10:40:11 +0200

    feat: added demos

--- demo/demo06_Fonction.c
+++ demo/demo06_Fonction.c
@@ -2,12 +2,12 @@
 int to_five(int n) {
     int a = 1;
     int b = 2;
-    return 5;
+    return a*a + b*b;
 }
 
 int main() {
     int c = 3;
     int d = 4;
     int e = to_five(d);
-    return c;
+    return e + c;
 }
